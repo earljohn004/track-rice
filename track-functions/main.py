e7685e05bea05a618d1631ad7fe2d666ef7b1867
earljohn004 <earl.abaquita@gmail.com>
2024-04-14 17:23:57 +0800

    working functions and firestore connection

--- track-functions/main.py
+++ track-functions/main.py
@@ -1,5 +1,7 @@
 # The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
+import datetime
 from firebase_functions import firestore_fn
+from google.cloud.firestore_v1.document import DocumentReference
 
 # The Firebase Admin SDK to access Cloud Firestore.
 from firebase_admin import initialize_app, firestore
@@ -7,29 +9,46 @@ import google.cloud.firestore
 
 app = initialize_app()
 
+FIELD_TIMESTAMP = "createdAt"
+FIELD_TOTAL_PRICE = "totalPrice"
+FIELD_TOTAL_SALES = "totalSales"
+COLLECTION_TRACKING = "tracking"
+
+
+def extract_document_id_from_timestamp(timestamp):
+    dt = datetime.datetime.fromtimestamp(timestamp)
+    return dt.strftime("%Y-%m-%d")
+
 
 @firestore_fn.on_document_created(document="sales/{pushId}")
 def addtotalsales(
     event: firestore_fn.Event[firestore_fn.DocumentSnapshot | None],
 ) -> None:
     """Listen for new documents created on sales/ collection.
-    Extract the timestamp and add to the corresponding timestamp for the day
+    Extract the timestamp and add to tracking summary.
+    Tracking contains sales total for the whole day
     """
     if event.data is None:
         return
 
     try:
-        # Get the timestamp
-        timestamp = event.data.get("createdAt")
-
-        # Get the totalcost
-        totalcost = event.data.get("totalPrice")
+        # Get the timestamp and totalcost
+        timestamp = event.data.get(FIELD_TIMESTAMP)
+        totalcost = event.data.get(FIELD_TOTAL_PRICE)
     except KeyError:
         return
 
     firestore_client: google.cloud.firestore.Client = firestore.client()
 
-    # Push the new message into Cloud Firestore using the Firebase Admin SDK.
-    _, doc_ref = firestore_client.collection("tracking").add(
-        {"timestamp": timestamp, "totalcost": totalcost}
+    document_id = extract_document_id_from_timestamp(timestamp)
+    document_reference = firestore_client.collection(COLLECTION_TRACKING).document(
+        document_id
     )
+
+    current_value = document_reference.get().get(FIELD_TOTAL_SALES)
+    total_sales = int(totalcost)
+
+    if current_value is not None:
+        total_sales = int(current_value) + int(totalcost)
+
+    document_reference.set({"timestamp": timestamp, FIELD_TOTAL_SALES: total_sales})
