15599a0cf16bf335290328e8dfda60d86c889784
Earl John Abaquita <earl.abaquita@gmail.com>
2024-04-14 20:19:28 +0800

    Revert "Add firebase functions to track total sales for the day (#2)" (#3)
    This reverts commit 944118f0cfa3995e4cdfb31168d4d3986f062f7c.

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,12 +1,5 @@
 import { AuthPage } from "@refinedev/mui";
 
 export const Login = () => {
-  return (
-    <AuthPage
-      type="login"
-      formProps={{
-        defaultValues: { email: "test@test.com", password: "123456" },
-      }}
-    />
-  );
+  return <AuthPage type="login" />;
 };
