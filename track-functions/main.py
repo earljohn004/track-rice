d32f99ebb9e4cd2c21ad7ed88aec041e988e6a53
earljohn004 <earl.abaquita@gmail.com>
2024-04-19 21:59:02 +0800

    fix functions

--- track-functions/main.py
+++ track-functions/main.py
@@ -1,23 +1,16 @@
 # The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
-import datetime
-from firebase_functions import firestore_fn
-from google.cloud.firestore_v1.document import DocumentReference
+from firebase_functions import firestore_fn, logger
 
 # The Firebase Admin SDK to access Cloud Firestore.
 from firebase_admin import initialize_app, firestore
 import google.cloud.firestore
 
-app = initialize_app()
-
-FIELD_TIMESTAMP = "timestamp"
+FIELD_CREATED = "createdAt"
 FIELD_TOTAL_PRICE = "totalPrice"
 FIELD_TOTAL_SALES = "totalSales"
 COLLECTION_TRACKING = "tracking"
 
-
-def extract_document_id_from_timestamp(timestamp):
-    dt = datetime.datetime.fromtimestamp(timestamp)
-    return dt.strftime("%Y-%m-%d")
+app = initialize_app()
 
 
 @firestore_fn.on_document_created(document="sales/{pushId}")
@@ -33,14 +26,14 @@ def addtotalsales(
 
     try:
         # Get the timestamp and totalcost
-        timestamp = event.data.get(FIELD_TIMESTAMP)
         totalcost = event.data.get(FIELD_TOTAL_PRICE)
+        document_id = event.data.get(FIELD_CREATED)
     except KeyError:
+        logger.error("Timestamp and Total price not found")
         return
 
     firestore_client: google.cloud.firestore.Client = firestore.client()
 
-    document_id = extract_document_id_from_timestamp(timestamp)
     document_reference = firestore_client.collection(COLLECTION_TRACKING).document(
         document_id
     )
